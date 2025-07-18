27f8e301fcd14f565578225fedef3da8e9595522
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2025-07-18 12:21:08 +0530

    fix: refactor

--- src/api/MobileEmission.ts
+++ src/api/MobileEmission.ts
@@ -1,13 +1,15 @@
 import { MobileRequest } from "../interfaces/mobileApi";
 import { makeApiRequest } from "../request";
+import { API_DOMAIN, MOBILE_API_PATH } from "../Constants";
 
 export async function calculate(
   payload: MobileRequest,
   useProxy: boolean = false
 ): Promise<string> {
+  
   const url = useProxy
-    ? '/v3/carbon/mobile'
-    : 'https://foundation-staging.agtech.ibm.com/v3/carbon/mobile';
+    ? MOBILE_API_PATH
+    : API_DOMAIN + MOBILE_API_PATH;
 
   return makeApiRequest<string>({
     method: 'POST',
