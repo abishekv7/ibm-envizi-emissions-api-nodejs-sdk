b98d90802497920a6592ad3e2091f84d77a27327
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2025-07-18 12:20:04 +0530

    fix: refactor

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export { Client } from './Client';
 export * as LocationApi from './api/LocationEmission';
 export * as FugitiveApi from './api/FugitiveEmission';
 export * as MobileApi from './api/MobileEmission';
-export * as Stationary from './api/StationaryEmission';
+export * as StationaryApi from './api/StationaryEmission';
 export * as GenericCalculation from './api/GenericCalculation';
