f3fefc28dd87e100acc157c00df112e1b25cc9f6
prakharsahu12 <prakhar.sahu@ibm.com>
2025-07-18 11:09:19 +0530

    added location

--- src/api/LocationEmission.ts
+++ src/api/LocationEmission.ts
@@ -1,10 +1,17 @@
 import { LocationRequest } from "../interfaces/locationApi";
 import { makeApiRequest } from "../request";
 
-export async function calculate(payload :LocationRequest): Promise<String> {
-  return makeApiRequest<String>({
+export async function calculate(
+  payload: LocationRequest,
+  useProxy: boolean = false
+): Promise<string> {
+  const url = useProxy
+    ? '/v3/carbon/location'
+    : 'https://foundation-staging.agtech.ibm.com/v3/carbon/location';
+
+  return makeApiRequest<string>({
     method: 'POST',
-    url: 'https://foundation-staging.agtech.ibm.com/v3/carbon/location',
-    data: payload
+    url,
+    data: payload,
   });
 }
