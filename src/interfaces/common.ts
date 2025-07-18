0a01838736ee800b09972f5b3105efc8b8df4d12
prakharsahu12 <prakhar.sahu@ibm.com>
2025-07-18 11:07:08 +0530

    added common interface

---
+++ src/interfaces/common.ts
@@ -0,0 +1,15 @@
+export interface Location {
+    country: string;
+    stateProvince?: string;
+    powerGrid?: string;
+}
+
+export interface Time {
+    date: string
+}
+
+export interface Activity {
+    type?: string;
+    value: number;
+    unit?: string; 
+}
