264b745f241bce31bdff9f09c30b1b24d8a8d743
prakharsahu12 <prakhar.sahu@ibm.com>
2025-07-18 11:07:28 +0530

    added interface for fugitive

---
+++ src/interfaces/fugitiveApi.ts
@@ -0,0 +1,10 @@
+import { Activity, Location , Time } from "./common";
+
+
+
+export interface FugitiveRequest {
+    location : Location;
+    time?: Time;
+    activity: Activity;
+    includeDetails: boolean;
+} 
