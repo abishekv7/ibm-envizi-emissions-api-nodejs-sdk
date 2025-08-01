18ca76fe59b2add9530c9098a28d67f2bdc763e3
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2025-08-01 17:49:40 +0530

    feat: added factor and factor/id API - factors

---
+++ src/api/Factors.ts
@@ -0,0 +1,36 @@
+import { Client } from "../Client";
+import { FACTOR_API_PATH, GET, POST } from "../Constants";
+import { FactorRequest } from "../interfaces/Api";
+import { makeApiRequest } from "../request";
+
+export async function retrieveFactor(
+  payload: FactorRequest,
+  useProxy: boolean = false
+): Promise<string> {
+  const client = Client.getInstance();
+  const url = useProxy
+    ? FACTOR_API_PATH
+    : client.getDomain() + FACTOR_API_PATH;
+
+  return makeApiRequest<string>({
+    method: POST,
+    url,
+    data: payload,
+  });
+}
+
+export async function getFactorById(
+    id: String,
+    useProxy: boolean = false
+  ): Promise<string> {
+  const client = Client.getInstance();
+  const path =`/${id}`;
+  const url = useProxy
+    ? FACTOR_API_PATH + path
+    : client.getDomain() + FACTOR_API_PATH + path;
+
+  return makeApiRequest<string>({
+    method: GET,
+    url
+  });
+}
