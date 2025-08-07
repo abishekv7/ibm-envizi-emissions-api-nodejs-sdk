73de7c93406127aee00aeb8b6f58369d8e228dcc
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2025-08-07 16:36:53 +0530

    feat: expose transportation and distribution API

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * as FugitiveApi from './api/FugitiveEmission';
 export * as MobileApi from './api/MobileEmission';
 export * as StationaryApi from './api/StationaryEmission';
 export * as GenericCalculation from './api/GenericCalculation';
+export * as TransportationDistributionApi from './api/TransportationDistributionEmission';
 export * as Factors from './api/Factors';
