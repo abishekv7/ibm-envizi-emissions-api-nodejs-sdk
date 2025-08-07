45cf811454cca132b7e1c9b8ebb183b1f94ed6da
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2025-08-07 16:36:12 +0530

    feat: added transportation and distribution API

---
+++ src/api/TransportationDistributionEmission.ts
@@ -0,0 +1,20 @@
+import { Client } from "../Client";
+import { TRANSPORTATION_AND_DISTRIBUTION_API_PATH, POST } from "../Constants";
+import { GenericCalculationRequest } from "../interfaces/Api";
+import { makeApiRequest } from "../request";
+
+export async function calculate(
+  payload: GenericCalculationRequest,
+  useProxy: boolean = false
+): Promise<string> {
+  const client = Client.getInstance();
+  const url = useProxy
+    ? TRANSPORTATION_AND_DISTRIBUTION_API_PATH
+    : client.getDomain() + TRANSPORTATION_AND_DISTRIBUTION_API_PATH;
+
+  return makeApiRequest<string>({
+    method: POST,
+    url,
+    data: payload,
+  });
+}
