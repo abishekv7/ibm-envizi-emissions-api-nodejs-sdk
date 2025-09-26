1de897825abbde65a88386ab94923956e497dd37
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2025-09-26 21:09:58 +0530

    fix: added tsdDoc-request.ts

--- src/request.ts
+++ src/request.ts
@@ -3,6 +3,30 @@ import { RequestConfig } from "./interfaces/Config";
 import { Client } from "./Client";
 import { CLIENT_SOURCE_HEADER } from "./Constants";
 
+/**
+ * Makes an API request with standardized headers and token refresh.
+ * 
+ * @export
+ * @template T The expected response data type
+ * @param {RequestConfig} config - The request configuration including method, URL, data, params, and optional headers
+ * @return {Promise<T>} A promise that resolves to the response data of type T
+ * @throws {Error} May throw an error if the API request fails or token refresh fails
+ * 
+ * @example
+ * interface UserData {
+ *   id: string;
+ *   name: string;
+ *   email: string;
+ * }
+ * 
+ * const userData = await makeApiRequest<UserData>({
+ *   method: 'GET',
+ *   url: 'https://api.example.com/users/123',
+ *   headers: { 'X-Custom-Header': 'value' }
+ * });
+ * console.log(userData.name); // Typed access to the response data
+ */
+
 export async function makeApiRequest<T>(config: RequestConfig): Promise<T> {
   const client = Client.getInstance();
   await client.refreshToken();
