a1cfa7930d947b93f6e25c8a6306499962c74170
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2025-12-09 22:37:59 +0530

    feat: added response interface - factorsets

--- src/api/FactorSets.ts
+++ src/api/FactorSets.ts
@@ -1,23 +1,24 @@
 import { Client } from "../Client";
 import { FACTOR_SET_API_PATH, GET } from "../Constants";
+import { FactorSetResponse } from "../interfaces/response/FactorSetResponse";
 import { makeApiRequest } from "../request";
 
 /**
  * Retrieves factor sets by making a GET request to the factor set API endpoint.
  * 
  * @export
- * @return {Promise<string>} A promise that resolves to the factor sets string returned by the API
+ * @return {Promise<FactorSetResponse>} A promise that resolves to the  FactorSetResponse returned by the API
  * @throws {Error} May throw an error if the API request fails
  * 
  * @example
  * const factorSets = await get();
  */
 export async function get(
-): Promise<string> {
+): Promise<FactorSetResponse> {
 const client = Client.getInstance();
   const url = client.getDomain() + FACTOR_SET_API_PATH;
 
-  return makeApiRequest<string>({
+  return makeApiRequest<FactorSetResponse>({
     method: GET,
     url
   });
