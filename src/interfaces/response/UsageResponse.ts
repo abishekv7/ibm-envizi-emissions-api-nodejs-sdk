e421619719cbc9a4b88626aac279559feae58a22
Abishek Venkatraman <Abishek.Venkatraman@ibm.com>
2025-12-16 22:10:28 +0530

    fix: updated docs for usage api

--- src/interfaces/response/UsageResponse.ts
+++ src/interfaces/response/UsageResponse.ts
@@ -5,12 +5,19 @@
 export interface UsageResponse {
     /** Organization ID for the API request */
     orgId ?: string;
+
+    /** Array of entitlement belonging to the Org */
     entitlement ?: Entitlement[]
 }
 
 
 export interface Entitlement {
+    /** Organization's billing start date*/
     startDate ?: string;
+
+    /** Organization's billing end date */
     endDate ?: string;
+
+    /** Total api calls made by the Organization in the billing cycle */
     totalApiCalls ?: number;
 }
