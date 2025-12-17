4d94b6910f4e2f73e2b427f4c96848856d383c94
Ridham Thumar <Ridham.Thumar@ibm.com>
2025-12-17 13:30:07 +0530

    fix: updating response of UnitResponse

--- src/interfaces/response/UnitResponse.ts
+++ src/interfaces/response/UnitResponse.ts
@@ -1,7 +1,17 @@
 /**
+ * Interface for unit object
+ */
+export interface Unit {
+  /** Unit symbol */
+  unit: string;
+  /** Unit name */
+  unitName: string;
+}
+
+/**
  * Interface for /units metadata API
  */
 export interface UnitResponse {
-  /** Array of string*/
-  units: string[];
+  /** Array of Unit objects */
+  units: Unit[];
 }
