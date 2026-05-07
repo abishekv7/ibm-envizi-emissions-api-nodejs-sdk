4ff96a401182e839c28d72bc01b9eae1578a1061
Santhosh-Ramalingam-T <santhosh.ramalingam.t@ibm.com>
2026-05-07 22:33:24 +0530

    feat: added unit and scope support for /type/seaarch and /factor/search - test

--- test/mocks/SearchRequest.ts
+++ test/mocks/SearchRequest.ts
@@ -16,4 +16,52 @@ const SearchPayload: SearchRequest = {
   }
 };
 
+// Search payload with unit parameter
+export const SearchPayloadWithUnit: SearchRequest = {
+  location: {
+    "country": "usa"
+  },
+  time: {
+    date: "2025-06-10",
+  },
+  activity: {
+    "search": "electricity",
+    "unit": "kWh"
+  }
+};
+
+// Search payload with scope parameter
+export const SearchPayloadWithScope: SearchRequest = {
+  location: {
+    "country": "usa"
+  },
+  time: {
+    date: "2025-06-10",
+  },
+  activity: {
+    "search": "natural gas",
+    "scope": "1"
+  }
+};
+
+// Search payload with both unit and scope parameters
+export const SearchPayloadWithBoth: SearchRequest = {
+  location: {
+    "country": "usa",
+    "stateProvince": "california"
+  },
+  time: {
+    date: "2025-06-10",
+  },
+  activity: {
+    "search": "electricity",
+    "unit": "MWh",
+    "scope": "2"
+  },
+  pagination:{
+    "page": 1,
+    "size": 10
+  }
+};
+
 export default SearchPayload;
